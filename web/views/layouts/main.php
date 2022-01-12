83d5af90fdc6256e92ad92c8a5c5cc2cba681f2a
agustinquintero <agustin.quintero@limayrac.fr>
2022-01-12 18:17:40 +0100

    MVC

---
+++ web/views/layouts/main.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ST-Service Technique</title>
+</head>
+<body>
+    <ul>
+        <li><a href="/">home</a></li>
+        <li><a href="/contact">contact</a></li>
+    </ul>
+    {{ content }}
+</body>
+</html>
