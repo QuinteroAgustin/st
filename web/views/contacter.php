140561984dfde13ea2ba861d60a7912d11b8e648
agustinquintero <agustin.quintero@limayrac.fr>
2022-01-18 16:47:43 +0100

    banniere

--- web/views/contacter.php
+++ web/views/contacter.php
@@ -8,34 +8,7 @@
 <!-- Slideshow -->
 <div class="w3-container">
     <div class="w3-display-container mySlides">
-      <img src="/img/accueil.jpg" style="width:100%">
-      <div class="w3-display-topleft w3-container w3-padding-32">
-        <span class="w3-white w3-padding-large w3-animate-bottom">Chauffage</span>
-      </div>
-    </div>
-    <div class="w3-display-container mySlides">
-      <img src="/img/clim.jpg" style="width:100%">
-      <div class="w3-display-middle w3-container w3-padding-32">
-        <span class="w3-white w3-padding-large w3-animate-bottom">Climatisation</span>
-      </div>
-    </div>
-    <div class="w3-display-container mySlides">
-      <img src="/img/froid.jpg" style="width:100%">
-      <div class="w3-display-topright w3-container w3-padding-32">
-        <span class="w3-white w3-padding-large w3-animate-bottom">Froid</span>
-      </div>
-    </div>
-
-    <!-- Slideshow next/previous buttons -->
-    <div class="w3-container w3-dark-grey w3-padding w3-xlarge">
-      <div class="w3-left" onclick="plusDivs(-1)"><i class="fa fa-arrow-circle-left w3-hover-text-teal"></i></div>
-      <div class="w3-right" onclick="plusDivs(1)"><i class="fa fa-arrow-circle-right w3-hover-text-teal"></i></div>
-    
-      <div class="w3-center">
-        <span class="w3-tag demodots w3-border w3-transparent w3-hover-white" onclick="currentDiv(1)"></span>
-        <span class="w3-tag demodots w3-border w3-transparent w3-hover-white" onclick="currentDiv(2)"></span>
-        <span class="w3-tag demodots w3-border w3-transparent w3-hover-white" onclick="currentDiv(3)"></span>
-      </div>
+      <img src="/img/banniere.png" style="width:100%">
     </div>
 </div>
 
